919a4ee11306976802608e5acd78cbd7f8e4394d
ps23aofford <ps23aofford@efcts.us>
2023-01-24 10:43:09 -0500

    Comments for data organization

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
+    //Naming Conventions
+    //userId = int primaryKey
+    //timeStamp = 00(day)00(month)0000(Time)
+    //logNames userID1xuserID2
+    //imageFolders userID
+    //imageNames ReciveUserID:TimeStamp
+    //imageLogEntry sentUserID:TimeStamp|
+    //logentry sentUserID:TimeStamp>messageText
+
+    //Tables
+    // User Table: user_id primaryKey, userName String, password String,
+    // OpenChats: user_id_one INT, user_id_two INT
+    
     echo "Hello";
 ?>
