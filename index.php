c12c9623918a42b0fc9f227d8f51745a50b0407a
Adam Offord <PS23AOfford@efcts.us>
2023-01-24 14:13:14 -0500

    Testing post request

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php 
-    //Naming Conventions
+    //Naming Conventionstactless apples
     //userId = int primaryKey
+    //sessionID = int primaryKey
     //timeStamp = 00(day)00(month)0000(Time)
-    //logNames userID1xuserID2
-    //imageFolders userID
-    //imageNames ReciveUserID:TimeStamp
-    //imageLogEntry sentUserID:TimeStamp|
+    //logNames = SessionID
+    //imageFolders = SessionID
+    //imageNames ImageName
+    //imageLogEntry sentUserID:TimeStamp|ImageName
     //logentry sentUserID:TimeStamp>messageText
 
     //Tables
-    // User Table: user_id primaryKey, userName String, password String,
-    // OpenChats: user_id_one INT, user_id_two INT
-    
-    echo "Hello";
+    // 'USERS' Table: user_id primaryKey, user_name String, password String,
+    // 'ChatSessions': session_id INT primaryKey, user_id_one INT, user_id_two INT
+    //
+    public function createUser(): void
+    {
+        $request_body = file_get_contents('php://input');
+        $data = json_decode($request_body, true);
+        $item = $data['item'];
+        echo(json_encode($item));
+         // Works!
+    }
 ?>
